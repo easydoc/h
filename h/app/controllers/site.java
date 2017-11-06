6e57aed90e74fba29599201cd86945d2a69e85c4
zhujl <zhujinlingcs@126.com>
2017-11-06 17:49:03 +0800

    创建项目

---
+++ h/app/controllers/site.java
@@ -0,0 +1,28 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+
+public class site extends Controller {
+
+    public static void index_ios() {
+        render();
+    }
+    
+    public static void person() {
+        render();
+    }
+    
+    public static void sense() {
+        render();
+    }
+    
+    public static void command() {
+        render();
+    }
+
+}
